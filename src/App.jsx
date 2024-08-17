0d3a2846e4af6c7164ed34f1cd654b7b94874a36
asheerali <asheerali1997@gmail.com>
2024-08-17 19:55:24 +0200

    million js added (need to be fixed)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Counter from './components/Counter/Counter.jsx';
-import Header from './components/Header.jsx';
-import { log } from './log.js';
-import ConfigureCounter from './components/Counter/ConfigureCounter.jsx';
+import Counter from "./components/Counter/Counter.jsx";
+import Header from "./components/Header.jsx";
+import { log } from "./log.js";
+import ConfigureCounter from "./components/Counter/ConfigureCounter.jsx";
 
 function App() {
-  log('<App /> rendered');
+  log("<App /> rendered");
 
   const [chosenCount, setChosenCount] = useState(0);
 
@@ -17,8 +17,8 @@ function App() {
     <>
       <Header />
       <main>
-      <ConfigureCounter onSet={handleSetCount}/>
-        <Counter initialCount={chosenCount} />
+        <ConfigureCounter onSet={handleSetCount} />
+        <Counter key={chosenCount} initialCount={chosenCount} />
         <Counter initialCount={0} />
       </main>
     </>
